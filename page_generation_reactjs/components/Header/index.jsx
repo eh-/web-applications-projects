fab27799a71f206002921e74230fb6a2c0c1f522
eh- <eh@Edwins-MBP.fios-router.home>
2023-08-23 22:01:21 -0500

    add header

---
+++ page_generation_reactjs/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+
+class Header extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+    <div className="cs142-header-section">
+        <h1>EH : HELLO WORLD</h1>
+    </div>
+    );
+  }
+}
+
+export default Header;
